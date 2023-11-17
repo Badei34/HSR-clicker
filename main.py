0dd0c32d88e17d163ba9e944ae53a9ce31bb9349
Deividas B <deividasbagdonas34@example.com>
2023-11-17 20:24:00 +0200

    Added files

---
+++ main.py
@@ -0,0 +1,59 @@
+from time import sleep
+import random
+import pyautogui as pgui
+import keyboard as key
+import vgamepad as vg
+
+gamepad = vg.VX360Gamepad()
+
+global in_dialogue
+in_dialogue = False
+
+
+def wait(): # Waits for random interval between 0, 0.3 and 0.6, 0.9 seconds
+    a = random.uniform(0, .3)
+    b = random.uniform(.6, .9)
+    sleep(random.uniform(a, b))
+
+
+def press_xbox(): # Performs A button press on xbox controller
+    gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+    gamepad.update()
+    sleep(.1)
+    gamepad.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+    gamepad.update()
+
+
+print("Press 'enter' to start the script")
+print("Press '1' to exit script.")
+print("Press '2' to pause/unpause script.")
+
+key.wait('enter')
+print("Starting...")
+
+while True: 
+    sleep(3)
+    if key.is_pressed('1'): # Exit
+        print("Exiting...")
+        exit()
+    
+    if key.is_pressed('2'): # Pause
+        print("Script paused.")
+        sleep(2)
+        key.wait('2')
+        print("Script unpaused.")
+        sleep(2)
+    
+    elif pgui.locateOnScreen("cont.png", confidence=0.8):
+        print("In dialogue.")
+        in_dialogue = True
+        
+        while in_dialogue: # If in dialogue continue clicking
+            if pgui.locateOnScreen("cont.png", confidence=0.8):
+                print("Clicking")
+                press_xbox()
+                wait()
+            else: # If not in dialogue exit clicking loop and continue searching
+                print("Not in dialogue.")
+                in_dialogue = False
+    
